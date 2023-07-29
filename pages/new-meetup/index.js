acc4070138df82f779247e725ff5dadcdb85e39a
Aditya <adityabhandari.chd@gmail.com>
2023-07-29 14:39:37 +0530

    ready for commit

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { Fragment } from "react";
+
+function NewMeetup() {
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(enteredMeetupData),
+    });
+    const data = await response.json();
+    console.log(data);
+    router.push("/");
+  }
+  return (
+    <Fragment>
+      <Head>
+        <title>Add new Meetup</title>
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
+}
+
+export default NewMeetup;
